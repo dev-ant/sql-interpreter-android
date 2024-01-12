4286a3e51bd56260497ffb5974a6e067f7c762db
Sanghyun Yi <njsharry32@gmail.com>
2024-01-12 14:45:45 +0900

    feature(utils): implement intent utility method

--- app/src/main/java/com/csapp/sqli/utils/IntentUtils.kt
+++ app/src/main/java/com/csapp/sqli/utils/IntentUtils.kt
@@ -1,4 +1,16 @@
 package com.csapp.sqli.utils
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+
 object IntentUtils {
+    fun navigateTo(
+        context: Context, destinationActivity: Class<*>, extras: Bundle? = null, flag: Int
+    ) {
+        val intent = Intent(context, destinationActivity)
+        extras?.let { intent.putExtras(it) }
+        intent.addFlags(flag)
+        context.startActivity(intent)
+    }
 }
