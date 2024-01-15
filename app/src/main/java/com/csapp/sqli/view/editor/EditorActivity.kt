90fab09288f05b01360e5f6231ed4bcc53a476f0
Sanghyun Yi <njsharry32@gmail.com>
2024-01-15 16:08:27 +0900

    feat(editor): add query editor activity and layout

---
+++ app/src/main/java/com/csapp/sqli/view/editor/EditorActivity.kt
@@ -0,0 +1,12 @@
+package com.csapp.sqli.view.editor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.csapp.sqli.R
+
+class EditorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editor)
+    }
+}
