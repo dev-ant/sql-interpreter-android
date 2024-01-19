486cacff0825f0d3d78c85c1975235bb3458bbe8
Sanghyun Yi <njsharry32@gmail.com>
2024-01-19 18:49:57 +0900

    feat(editor): implement editor left side line counter

--- app/src/main/java/com/csapp/sqli/view/editor/EditorActivity.kt
+++ app/src/main/java/com/csapp/sqli/view/editor/EditorActivity.kt
@@ -1,52 +1,53 @@
 package com.csapp.sqli.view.editor
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
-import android.text.Spannable
+import android.text.Layout
 import android.text.TextWatcher
-import android.text.style.ForegroundColorSpan
 import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.csapp.sqli.R
+import java.lang.StringBuilder
 
 class EditorActivity : AppCompatActivity() {
 
     private lateinit var editText: EditText
+    private lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
 
         editText = findViewById(R.id.edittext_query_editor)
+        textView = findViewById(R.id.textview_query_editor_liner)
 
         editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                // Not yet implemented
+                // "Not yet implemented"
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                // Not yet implemented
+                // "Not yet implemented"
             }
 
-            override fun afterTextChanged(editable: Editable) {
-                println("askjdnaksd")
-                val keyword = "SELECT"
-                highlightKeyword(editable, keyword, Color.RED)
+            override fun afterTextChanged(p0: Editable?) {
+                val lineCount = getLineCount(editText.text.toString())
+                setEditorLiner(lineCount)
             }
         })
     }
+    private fun getLineCount(text: String): Int {
+        val layout: Layout = editText.layout
+        val text = editText.text
+        return layout.getLineForOffset(text.length) + 1
+    }
 
-    private fun highlightKeyword(editable: Editable, keyword: String, color: Int) {
-        val spans = editable.getSpans(0, editable.length, ForegroundColorSpan::class.java)
-
-        for (span in spans) {
-            editable.removeSpan(span)
+    private fun setEditorLiner(count: Int) {
+        var stringBuilder = StringBuilder()
+        for(i in 1..count) {
+            stringBuilder.append("$i\n")
         }
 
-        var index = editable.toString().indexOf(keyword)
-        while (index >= 0) {
-            editable.setSpan(ForegroundColorSpan(color), index, index + keyword.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-            index = editable.toString().indexOf(keyword, index + keyword.length)
-        }
+        textView.text = stringBuilder.toString()
     }
 }
