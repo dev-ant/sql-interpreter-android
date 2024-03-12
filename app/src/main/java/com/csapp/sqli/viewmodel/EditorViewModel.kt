dbf55222387af3476fca90a6138c9d6c208853c9
Lee Sanghyun <59863112+Nter-developer@users.noreply.github.com>
2024-03-13 00:32:24 +0900

    [gui] Redesign query execute result table and theme (#26)
    * gui(editor): add scroll view to wrap result table
    
    * feat(*): add theme default font size and padding values
    
    * refactor(editor): update font size and padding value to theme default
    
    * fix(editor): allow to use small letter 'select' to identify query
    
    * fix(util): add result display fun to react to screen width

--- app/src/main/java/com/csapp/sqli/viewmodel/EditorViewModel.kt
+++ app/src/main/java/com/csapp/sqli/viewmodel/EditorViewModel.kt
@@ -5,11 +5,8 @@ import com.csapp.sqli.DatabaseHelper
 
 class EditorViewModel(private val databaseHelper: DatabaseHelper) : ViewModel() {
 
-    // private val _toDoList = MutableLiveData<List<ToDo>>()
-    // val toDoList: LiveData<List<ToDo>> get() = _toDoList
-
-    fun executeQuery(sql: String): Any? {
-        return if (sql.startsWith("SELECT")) {
+    fun executeQuery(sql: String): Any {
+        return if (sql.startsWith("SELECT") or sql.startsWith("select")) {
             databaseHelper.execQueryReturn(sql)
         } else {
             databaseHelper.execQueryNoReturn(sql)
