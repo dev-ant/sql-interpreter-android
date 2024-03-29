7434e842c5d914fc3080047b4b7a4d8404dab7da
Sanghyun Yi <njsharry32@gmail.com>
2024-03-29 15:56:46 +0900

    test(editor): add unit test code for line number

--- app/src/test/java/com/csapp/sqli/viewmodel/EditorViewModelTest.kt
+++ app/src/test/java/com/csapp/sqli/viewmodel/EditorViewModelTest.kt
@@ -1,5 +1,45 @@
 package com.csapp.sqli.viewmodel
 
-import org.junit.jupiter.api.Assertions.*
+import android.widget.EditText
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.csapp.sqli.repository.DatabaseRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
 
-class EditorViewModelTest
+@RunWith(MockitoJUnitRunner::class)
+class EditorViewModelTest {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var viewModel: EditorViewModel
+    private lateinit var databaseRepository: DatabaseRepository
+
+    @Before
+    fun setUp() {
+        databaseRepository = mockk()
+        viewModel = EditorViewModel(databaseRepository)
+    }
+
+    @Test
+    fun `onStatementChanged should update lineNumberView with correct line numbers`() {
+        // Given
+        val expectedLineNumbers = "1\n2\n3\n" // Assuming 3 lines of text
+        val mockEditText =
+            mockk<EditText>().apply {
+                every { layout.lineCount } returns 3
+            }
+        viewModel.statementEditView.value = mockEditText
+
+        // When
+        viewModel.onStatementChanged()
+
+        // Then
+        assertEquals(expectedLineNumbers, viewModel.lineNumberView.value)
+    }
+}
