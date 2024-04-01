e8433377868adc438d2e113fbe1c0b0db7f3b37e
Sanghyun Yi <njsharry32@gmail.com>
2024-04-01 11:05:39 +0900

    infra(ci/cd): configure github actions unit test workflow

--- app/src/test/java/com/csapp/sqli/viewmodel/EditorViewModelTest.kt
+++ app/src/test/java/com/csapp/sqli/viewmodel/EditorViewModelTest.kt
@@ -3,7 +3,7 @@ package com.csapp.sqli.viewmodel
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
-internal class EditorViewModelTest {
+class EditorViewModelTest {
     @Test
     fun onStatementChangedTest() {
         Assertions.assertEquals(4, 2 + 2)
