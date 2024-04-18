efdd487db38150fd0d8f0b7c46bdee90fb60e973
Sanghyun Yi <njsharry32@gmail.com>
2024-04-18 22:21:32 +0900

    refactor(editor): separate statement changed fun to except handling and update

--- app/src/main/java/com/csapp/sqli/viewmodel/EditorViewModel.kt
+++ app/src/main/java/com/csapp/sqli/viewmodel/EditorViewModel.kt
@@ -1,5 +1,7 @@
 package com.csapp.sqli.viewmodel
 
+import android.text.Layout
+import android.util.Log
 import android.widget.EditText
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -8,26 +10,35 @@ import com.csapp.sqli.model.LineNumber
 import com.csapp.sqli.repository.DatabaseRepository
 
 class EditorViewModel(private val databaseRepository: DatabaseRepository) : ViewModel() {
-    private val lineNumberModel = LineNumber("")
+    private val modelLineNumber = LineNumber()
 
     private val _editTextStatement = MutableLiveData<EditText>()
-    val editTextStatement: LiveData<EditText> = _editTextStatement
+    val editTextStatement: MutableLiveData<EditText> = _editTextStatement
 
     private val _textViewLineNumber = MutableLiveData<String>()
-    val lineNumberView:LiveData<String> = _textViewLineNumber
+    val textViewLineNumber: LiveData<String> = _textViewLineNumber
 
+    // Data binding with Activity_editor.xml #EditText afterTextChanged()
     fun onStatementChanged() {
-        val editText = statementEditView.value
+        val editText = editTextStatement.value
         val layout = editText?.layout
         layout?.let {
-            lineNumberModel.content = generateLineNumber(it.lineCount)
-            lineNumberView.value = lineNumberModel.content
+            updateLineNumbers(it)
         }
     }
 
+    private fun updateLineNumbers(it: Layout) {
+        if (it.lineCount != modelLineNumber.number) {
+            modelLineNumber.content = generateLineNumber(it.lineCount)
+            modelLineNumber.number = it.lineCount
+            _textViewLineNumber.value = modelLineNumber.content
+        }
+    }
+
+    // Activity_editor.xml과 data binding #버튼 클릭시 실행
     fun execStatement(): Any? {
-        return statementEditView.value?.let {
-            if (isQueryStatement(statementEditView.value.toString())) {
+        return editTextStatement.value?.let {
+            if (isQueryStatement(editTextStatement.value.toString())) {
                 databaseRepository.execQuery(it.text.toString())
             } else {
                 databaseRepository.execStatement(it.text.toString())
@@ -35,10 +46,12 @@ class EditorViewModel(private val databaseRepository: DatabaseRepository) : View
         }
     }
 
+    // SQL문이 쿼리인지 검증. 즉 SELECT로 시작하는지 확인
     private fun isQueryStatement(statement: String): Boolean {
         return statement.startsWith("SELECT") or statement.startsWith("select")
     }
 
+    // Line Count를 입력받으면 Line Number을 생성
     private fun generateLineNumber(count: Int): String {
         val stringBuilder = StringBuilder()
         for (i in 1..count) {
