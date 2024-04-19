137869f60a0cd314545030053904d9a4162cbc83
Lee Sanghyun <59863112+dev-ant@users.noreply.github.com>
2024-04-19 13:11:00 +0900

    [refactor] Separate line number logic from editor view model (#46)
    * refactor(util): add line number manager class
    
    * refactor(editor): separate line number logic to util
    
    * refactor(editor): reduce line number update logic call
    
    * refactor(editor): separate render fun to after text changed fun

---
+++ app/src/main/java/com/csapp/sqli/utils/LineNumberManager.kt
@@ -0,0 +1,27 @@
+package com.csapp.sqli.utils
+
+import android.text.Layout
+import com.csapp.sqli.model.LineNumber
+
+object LineNumberManager {
+    private val modelLineNumber = LineNumber()
+
+    // If line number changed, update model and render line number text view
+    fun updateLineNumbers(it: Layout): String? {
+        if (it.lineCount != modelLineNumber.number) {
+            modelLineNumber.content = generateLineNumber(it.lineCount)
+            modelLineNumber.number = it.lineCount
+            return modelLineNumber.content
+        }
+        return null
+    }
+
+    // Generate line number like ""1/n2/n3/n ... count"
+    private fun generateLineNumber(count: Int): String {
+        return buildString {
+            for (i in 1..count) {
+                append("$i\n")
+            }
+        }
+    }
+}
--- app/src/main/java/com/csapp/sqli/viewmodel/EditorViewModel.kt
+++ app/src/main/java/com/csapp/sqli/viewmodel/EditorViewModel.kt
@@ -5,12 +5,10 @@ import android.widget.EditText
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.csapp.sqli.model.LineNumber
 import com.csapp.sqli.repository.DatabaseRepository
+import com.csapp.sqli.utils.LineNumberManager
 
 class EditorViewModel(private val databaseRepository: DatabaseRepository) : ViewModel() {
-    private val modelLineNumber = LineNumber()
-
     private val _editTextStatement = MutableLiveData<EditText>()
     val editTextStatement: MutableLiveData<EditText> = _editTextStatement
 
@@ -19,23 +17,22 @@ class EditorViewModel(private val databaseRepository: DatabaseRepository) : View
 
     // Data binding with Activity_editor.xml #afterTextChanged()
     fun onStatementChanged() {
-        val editText = editTextStatement.value
-        editText?.layout?.let { updateLineNumbers(it) }
+        editTextStatement.value?.layout?.let { layout ->
+            renderLineNumberView(layout)
+        }
     }
 
-    // If line number changed, update model and render line number text view
-    private fun updateLineNumbers(it: Layout) {
-        if (it.lineCount != modelLineNumber.number) {
-            modelLineNumber.content = generateLineNumber(it.lineCount)
-            modelLineNumber.number = it.lineCount
-            _textViewLineNumber.value = modelLineNumber.content
+    // Render line number view when line number updated
+    private fun renderLineNumberView(layout: Layout) {
+        LineNumberManager.updateLineNumbers(layout)?.let { lineNumber ->
+            _textViewLineNumber.value = lineNumber
         }
     }
 
     // View binding with Activity_editor.xml, Check Editor Activity
     fun execStatement(): Any? {
         return _editTextStatement.value?.let {
-            if (isQueryStatement(editTextStatement.value.toString())) {
+            if (isSelectStatement(editTextStatement.value.toString())) {
                 databaseRepository.execQuery(it.text.toString())
             } else {
                 databaseRepository.execStatement(it.text.toString())
@@ -44,16 +41,7 @@ class EditorViewModel(private val databaseRepository: DatabaseRepository) : View
     }
 
     // Check if a statement starts with "SELECT" ignore case
-    private fun isQueryStatement(statement: String): Boolean {
+    private fun isSelectStatement(statement: String): Boolean {
         return statement.startsWith("SELECT", ignoreCase = true)
-    }
-
-    // Generate line number like ""1/n2/n3/n ... count"
-    private fun generateLineNumber(count: Int): String {
-        val stringBuilder = StringBuilder()
-        for (i in 1..count) {
-            stringBuilder.append("$i\n")
-        }
-        return stringBuilder.toString()
     }
 }
